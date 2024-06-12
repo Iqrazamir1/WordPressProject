650bd67e88e17a248ebf716c4036fb002221e558
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-06-12 15:48:19 +0100

    Update git-automate.sh
    Updated git script

--- git-automate.sh
+++ git-automate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt -y install git 
-cd /root/
+sudo cd /root/
 sudo git clone https://github.com/Iqrazamir1/WordPressProject.git
-chmod -R 755 WordPressProject
+sudo chmod -R 755 WordPressProject
 sudo bash WordPressProject/lemp-automate.sh
