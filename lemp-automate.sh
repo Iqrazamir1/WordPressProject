a43380507d44c66509fc596fa060ac188792c5e9
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-06-12 15:31:17 +0100

    Update lemp-automate.sh
    Updated the unit test commands

--- lemp-automate.sh
+++ lemp-automate.sh
@@ -3,19 +3,19 @@ sudo apt -y update
 sudo apt -y upgrade
 
 # This file will contain the output of our LEMP stack unit tests.
-sudo touch testing.txt
+sudo touch /root/testing.txt
 
 sudo apt -y install nginx
 
 # Starts and enables nginx on a server reboot. The 2nd command will only run if the first command is successful. 
 sudo systemctl start nginx && sudo systemctl enable nginx 
-sudo systemctl start nginx > testing.txt
+sudo systemctl start nginx > /root/testing.txt
 
 # Install Mariadb modules 
 sudo apt -y install mariadb-server
 sudo systemctl start mariadb && sudo systemctl enable mariadb
-sudo systemctl start mariadb >> testing.txt
+sudo systemctl start mariadb >> /root/testing.txt
 
 # Install PHP modules 
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
-sudo php -v >> testing.txt
+sudo php -v >> /root/testing.txt
