9ae020d04cf72ab25a1920ca17224e36de850ab6
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-06-19 15:07:36 +0100

    Update Wordpress-install.sh
    Step-by-Step Instructions

--- Wordpress-install.sh
+++ Wordpress-install.sh
@@ -1,54 +1,38 @@
 #!/bin/bash
 
-sudo cd /var/www/html
-sudo apt -y install unzip
+# Step 1: Install WordPress
+wget https://wordpress.org/latest.zip
+sudo apt install unzip
+sudo unzip latest.zip -d /var/www/html/
 
-# Installs latest version of WordPress 
-sudo wget https://wordpress.org/latest.zip
-
-# Unzip wordpress file
-sudo unzip latest.zip
-
-# Removes the file (clean up)
-rm latest.zip
-
-# 
+# Step 2: Create a Database and User for WordPress Site
 sudo mariadb -u root
-
-# Creates Mariadb database called wordpress 
 create database wordpress;
-
-# Creates a user for the database (Football1 is the password)
-create user wpuser@localhost identified by 'Football1';
-
-# Grant all privileges to the user
+create user wpuser@localhost identified by 'your-password';
 grant all privileges on wordpress.* to wpuser@localhost;
-
-# Apllies everything I've done 
 flush privileges;
 exit;
 
-# Creates a wp-cofig.php which wordpress will use as the defaulf config 
-cp wp-config-sample.php wp-config.php
+# Step 3: Configure WordPress
+cd /var/www/html/wordpress/
+sudo cp wp-config-sample.php wp-config.php
+sudo vim wp-config.php
 
-# Edit config file
-vim wp-config.php
+Edit the below in the wp-config.php file
 
-# Only group user can read the file
+  /** The name of the database for WordPress */
+  define('DB_NAME', 'wordpress');
+
+  /** MySQL database username */
+  define('DB_USER', 'wpuser');
+
+  /** MySQL database password */
+  define('DB_PASSWORD', 'password_here');
+
 sudo chmod 640 wp-config.php
+sudo chown www-data:www-data /var/www/html/wordpress/ -R
 
-# 
-sudo cd /etc/nginx/conf.d/ 
-sudo touch wordpress.conf (pull from s3bucket)
-
-
-
-
-
-
-
-
-
-
-
-
+# Step 4: Create an Nginx Server Block for WordPress
+sudo vim /var/www/html/wordpress/ # Update the server_name and root (The root path is: /var/www/html/wordpress/;)
+sudo nginx -t
+sudo systemctl reload nginx
