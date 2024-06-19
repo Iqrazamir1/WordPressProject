a42db27df87f0dc79ae924b8862247ce30a43731
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-06-19 10:40:10 +0100

    Update git-automate.sh

--- git-automate.sh
+++ git-automate.sh
@@ -4,3 +4,9 @@ sudo cd /root/
 sudo git clone https://github.com/Iqrazamir1/WordPressProject.git
 sudo chmod -R 755 WordPressProject
 sudo bash WordPressProject/lemp-automate.sh
+
+# Change directory to /var/www/html/
+cd /var/www/html/
+ 
+# Rename the default Apache test page if it exists
+sudo mv index.html index.html.old
