982ed4a7e8fceb856a38d16b5e350ec07af2e528
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-07-10 10:53:28 +0100

    Update script-s3-bucket.sh
    Updated S3 bucket link

--- script-s3-bucket.sh
+++ script-s3-bucket.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo wget -O /root/git-setup.sh https://saxtonator-bucket.s3.amazonaws.com/git-setup.sh
+sudo wget -O /root/git-setup.sh https://wordpress-bucket-s3.s3.eu-north-1.amazonaws.com/git-automate.sh
 sudo chmod 755 git-setup.sh
 sudo bash git-setup.sh
