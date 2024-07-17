5a2aa4ad56658eb91b1eb7f76efb9d92d85e42cb
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-07-17 14:41:55 +0100

    Update script-s3-bucket.sh
    Updated file name

--- script-s3-bucket.sh
+++ script-s3-bucket.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo wget -O /root/git-setup.sh https://wordpress-bucket-s3.s3.eu-north-1.amazonaws.com/git-automate.sh
-sudo chmod 755 git-setup.sh
-sudo bash git-setup.sh
+sudo wget -O /root/git-automate.sh https://wordpress-bucket-s3.s3.eu-north-1.amazonaws.com/git-automate.sh
+sudo chmod 755 git-automate.sh
+sudo bash git-automate.sh
